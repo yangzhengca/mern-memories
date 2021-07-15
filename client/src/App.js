9ec4ee454843ca40dcd95f5f62dcfb1c5673f245
yangzhengca <yangzhengca@gmail.com>
2021-07-15 12:29:42 -0600

    before deployed

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,57 @@
-import React, { useEffect} from 'react'
-import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
+import React, { useEffect, useState } from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
-import { useDispatch } from 'react-redux';
-import { getPosts } from './actions/posts'
+import { useDispatch } from "react-redux";
+import { getPosts } from "./actions/posts";
 
-import memories from './images/memories.jpg'
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import useStyles from './styles';
+import memories from "./images/memories.jpg";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from "./styles";
 
 const App = () => {
-    const classes=useStyles()
-    const dispatch=useDispatch()
+  const [currentId, setCurrentId] = useState(null);
 
-    useEffect(()=>{
-        dispatch(getPosts())
-    },[dispatch])
+  const classes = useStyles();
+  const dispatch = useDispatch();
 
-    return (
-        <Container maxWidth='lg'>
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant='h2' align="center">
-                    Memories
-                </Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60" />
-            </AppBar>
-            <Grow in>
-                <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
-                    <Grid item xs={12} sm={7}>
-                        <Posts />
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <Form />
-                    </Grid>
-                </Grid>
-            </Grow>        
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            className={classes.mainContainer}
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
         </Container>
-    )
-}
+      </Grow>
+    </Container>
+  );
+};
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from "mongoose"
 import cors from 'cors'
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js'
 
 
 const app = express()
+dotenv.config()
 
 
 app.use(bodyParser.json({limit:"30mb",extended:true}))
@@ -14,9 +15,12 @@ app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
 app.use(cors())
 app.use('/posts',postRoutes)
 
-const DB_URL='mongodb+srv://db@cluster0.m1pot.mongodb.net/mern-memo?retryWrites=true&w=majority'
+app.get('/',(req,res)=>{
+    res.send('Welcome to Memories API')
+})
 
 const PORT= process.env.PORT || 5000
+const DB_URL=process.env.DB_URL
 
 mongoose.connect(DB_URL,{ useNewUrlParser:true, useUnifiedTopology:true})
     .then(()=>app.listen(PORT,()=>console.log(`Server running on port: ${PORT}`)))
